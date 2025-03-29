f4d9dea58e650bf352eb2500a49983ae38391dbd
pat2000kr@gmail.com <scene83@daum.net>
2025-03-29 15:21:15 +0900

    [COMMIT] 팀피드 코드 추가
    팀 피드 구현 중

--- src/utils/teamFeedLocators.py
+++ src/utils/teamFeedLocators.py
@@ -4,10 +4,14 @@ from selenium.webdriver.common.by import By
 
 
 TEAM_FEED_TAB = (By.XPATH, "//a[@href='/teams/1']")
-TEAM_PROFILE_CHANGE = (By.XPATH, "//div[@class='cursor-pointer']")
+TEAM_PROFILE_CHANGE = (By.XPATH, "//*[local-name()='svg' and @class='cursor-pointer']")
 TEAM_MENU_PLUS = (By.CLASS_NAME, "lucide lucide-plus")
 TEAM_SAME_MENU = (By.XPATH, "//button[text()='같은 메뉴 먹기']")
 TEAM_BUTTON_DAWN = (By.XPATH, "//button[text()='▼']")
+TEXTAREA_LIKE = (By.NAME, "pros")
+TEXTAREA_HATE = (By.NAME, "cons")
+CLOSE_BUTTON = (By.CLASS_NAME, "text-2xl.cursor-pointer")
+PROFILE_CHANGE_BTN = (By.XPATH, "//button[text()='프로필 수정 완료']")
 
 # 드롭다운 / test_register_002, test_register_019
 DROP_DOWN = (By.XPATH, "//button[@role='combobox']")
--- tests/test_teamFeed.py
+++ tests/test_teamFeed.py
@@ -4,6 +4,7 @@ import time
 import pytest
 from src.pages.teamFeed import teamFeed
 from src.utils import teamFeedLocators
+from selenium.webdriver.common.keys import Keys
 
 
 def test_teamFeed_001(createDriver): # 함수명 바꾸셔도 좋아요
@@ -40,9 +41,37 @@ def test_teamFeed_006(createDriver):
     myTeamFeed.goToPage("팀 피드")
     myTeamFeed.getElement(teamFeedLocators.DROP_DOWN).click()
     myTeamFeed.getElement(teamFeedLocators.DROP_OPTION_DESIGN_2).click() #디자인 2팀
-@pytest.mark.sh      
+
+
 def test_teamFeed_007(createDriver):
     myTeamFeed = teamFeed(createDriver)
     myTeamFeed.goToPage("팀 피드")
-    myTeamFeed.getElement(teamFeedLocators.TEAM_PROFILE_CHANGE).click()
+    myTeamFeed.getElement(teamFeedLocators.TEAM_PROFILE_CHANGE).click() #프로필 수정 팝업 진입
     time.sleep(2)
+         
+def test_teamFeed_008(createDriver):
+    myTeamFeed = teamFeed(createDriver)
+    myTeamFeed.goToPage("팀 피드")
+    myTeamFeed.getElement(teamFeedLocators.TEAM_PROFILE_CHANGE).click() 
+    myTeamFeed.getElement(teamFeedLocators.CLOSE_BUTTON).click() #닫기
+    time.sleep(1)
+    
+@pytest.mark.sh 
+def test_teamFeed_009(createDriver): #10자 미만 텍스트 입력
+    myTeamFeed = teamFeed(createDriver)
+    myTeamFeed.goToPage("팀 피드")
+    myTeamFeed.getElement(teamFeedLocators.TEAM_PROFILE_CHANGE).click()
+    myTeamFeed.getElement(teamFeedLocators.TEXTAREA_LIKE).send_keys(Keys.CONTROL, 'a')
+    myTeamFeed.getElement(teamFeedLocators.TEXTAREA_LIKE).send_keys("다좋아요") #10자 미만 텍스트 입력
+    myTeamFeed.getElement(teamFeedLocators.PROFILE_CHANGE_BTN).click()
+    time.sleep(1)
+    
+def test_teamFeed_010(createDriver): #100자 이상 텍스트 입력
+    myTeamFeed = teamFeed(createDriver)
+    myTeamFeed.goToPage("팀 피드")
+    myTeamFeed.getElement(teamFeedLocators.TEAM_PROFILE_CHANGE).click()
+    myTeamFeed.getElement(teamFeedLocators.TEXTAREA_LIKE).send_keys(Keys.CONTROL, 'a')
+    myTeamFeed.getElement(teamFeedLocators.TEXTAREA_LIKE).send_keys("매콤하면서도 깊은 감칠맛이 있는 음식은 좋아요! 특히 고추장 베이스의 한식이나 향신료가 풍부한 태국 요리는 식욕을 돋우는 맛의 조화가 완벽해서 자주 찾게 됩니다. 신선한 해산물이 들어간 요리도 좋아하는데, 바다의 깊은 맛이 입안에 퍼질 때의 그 만족감이 특별합니다.") 
+    myTeamFeed.getElement(teamFeedLocators.PROFILE_CHANGE_BTN).click()
+    time.sleep(1)
+    
